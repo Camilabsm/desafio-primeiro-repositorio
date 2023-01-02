59692092532bf8d6ebcca08ce43d27ec063d9220
Camilabsm <camilabarbosasm@gmail.com>
2023-01-02 12:46:38 -0300

    exemplo operador ternario

--- ProgramaçãoTestes_Java/tipos-e-variaveis/src/TiposVariaveis.java
+++ ProgramaçãoTestes_Java/tipos-e-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,13 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        
+    int a, b;
+    a = 5;
+    b = 6;
+
+    String resultado = a+b==11 ? "verdadeiro" : "falso";
+
+    System.out.println(resultado);
+
     }
 }
