841cbd9471590dbf0f0ce743e38ca5bec3bd8dce
Camilabsm <camilabarbosasm@gmail.com>
2023-01-21 11:15:16 -0300

    aula testes com rest assured

---
+++ TestesPraticos/project/src/test/java/project/example/User/UserTests.java
@@ -0,0 +1,26 @@
+package project.example.User;
+
+import io.restassured.RestAssured;
+import io.restassured.filter.log.ErrorLoggingFilter;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.*;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.config.LogConfig.logConfig;
+import static io.restassured.module.jsv.JsonSchemaValidator.*;
+import static org.hamcrest.Matchers.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+
+public class UserTests {
+
+    @BeforeAll
+    void setup(){
+        RestAssured.baseURI = "https://petstore.swagger.io/v2";
+    }
+    
+}
